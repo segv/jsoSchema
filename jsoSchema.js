d8dd55d2780657df23ae6c843903b649471a13ac
Marco Baringer <mb@bese.it>
2013-10-12 20:23:22 +0200

    Refactor jsoSchema.js to avoid repeating jsoSchema. all over the place

--- jsoSchema.js
+++ jsoSchema.js
@@ -5,216 +5,216 @@
  * second function to call if the value fails.
  */
 
-var jsoSchema = { };
+var jsoSchema = (function () {
 
-/** @typedef {function(*, continuation)} */
-jsoSchema.validator;
+    var s = { };
 
-/**
- * Short circuiting and.
- *
- * @nosideeffects
- * @param {Array.<jsoSchema.validator>} conditions
- * @return {validator}
- */
-jsoSchema.Every = function Every (conditions) {
-    if (conditions.length == 0) {
-        return function (value, p, f) { return p(); };
-    } else {
-        return function (value, p, f) {
-            return conditions[0](value,
-                                 function () { 
-                                     return jsoSchema.Every(conditions.slice(1))(value, p, f); 
-                                 },
-                                 f);
-        };
-    }
-};
+    /** @typedef {function(*, continuation)} */
+    var validator;
 
-jsoSchema.And = function And (a, b) {
-    return jsoSchema.Every([a,b]);
-};
-
-/**
- * Short circuiting or
- *
- * @nosideeffects
- * @param {Array.<validator>} conditions
- * @return {validator}
- */
-jsoSchema.Any = function Any (conditions) {
-    if (conditions.length == 0) {
-        return function (value, p, f) {
-            return f("Testing ", value, "against an empty Any list");
-        };
-    } else {
-        return function (value, p, f) {
-            return conditions[0](value,
-                                 function () {
-                                     return p();
-                                 },
-                                 function () {
-                                     return jsoSchema.Any(conditions.slice(1))(value, p, f);
-                                 });
-        };
-    }
-};
-
-/**
- * 2 argument Any
- *
- * @nosideeffects
- * @param {validator} a
- * @param {validator} b
- * @return {validator}
- */
-jsoSchema.Or = function Or (a, b) {
-    return jsoSchema.Any([ a, b ]); 
-};
-
-jsoSchema.If = function If (condition, then, els) {
-    return jsoSchema.Or(jsoSchema.And(condition, then), els);
-};
-
-/** 
- * Helper function for simple tests
- * @param {function(*):boolean} valueCheck
- * @return {validator}
- */
-jsoSchema.Condition = function Condition (valueCheck) {
-    return function(value, p, f) {
-        return valueCheck(value) ? p() : f(value, " did not return true from ", valueCheck);
-    };
-};
-
-/**
- * Return type, as string, of the passed in object. Distinguish
- * between null, arrays and objects.
- *
- * as per http://javascript.crockford.com/remedial.html
- *
- * @param {*} value
- * @return {string}
- */
-jsoSchema.typeOf = function typeOf(value) {
-    var s = typeof value;
-    if (s === 'object') {
-        if (value) {
-            if (Object.prototype.toString.call(value) == '[object Array]') {
-                return 'array';
-            }
+    /**
+     * Short circuiting and.
+     *
+     * @nosideeffects
+     * @param {Array.<jsoSchema.validator>} conditions
+     * @return {validator}
+     */
+    s.Every = function Every (conditions) {
+        if (conditions.length == 0) {
+            return function (value, p, f) { return p(); };
         } else {
-            return 'null';
+            return function (value, p, f) {
+                return conditions[0](value,
+                                     function () { 
+                                         return s.Every(conditions.slice(1))(value, p, f); 
+                                     },
+                                     f);
+            };
         }
-    }
-    return s;
-};
+    };
 
-/**
- * @nosideeffects
- * @param {typeName} string
- * @return {validator}
- */
-jsoSchema.OfType = function OfType (typeName) {
-    return jsoSchema.Condition(function (value) { return jsoSchema.typeOf(value) == typeName; });
-};
+    s.And = function And (a, b) {
+        return s.Every([a,b]);
+    };
 
-/**
- * @nosideeffects
- * @return {validator}
- */
-jsoSchema.Number = function Number () {
-    return jsoSchema.OfType("number");
-};
+    /**
+     * Short circuiting or
+     *
+     * @nosideeffects
+     * @param {Array.<validator>} conditions
+     * @return {validator}
+     */
+    s.Any = function Any (conditions) {
+        if (conditions.length == 0) {
+            return function (value, p, f) {
+                return f("Testing ", value, "against an empty Any list");
+            };
+        } else {
+            return function (value, p, f) {
+                return conditions[0](value,
+                                     function () {
+                                         return p();
+                                     },
+                                     function () {
+                                         return s.Any(conditions.slice(1))(value, p, f);
+                                     });
+            };
+        }
+    };
 
-/**
- * @nosideeffects
- * @param {number} lowerBound
- * @return {validator}
- */
-jsoSchema.GreaterThan = function GreaterThan (lowerBound) {
-    return jsoSchema.And(jsoSchema.Number(), jsoSchema.Condition(function (value) { return lowerBound < value; }));
-};
+    /**
+     * 2 argument Any
+     *
+     * @nosideeffects
+     * @param {validator} a
+     * @param {validator} b
+     * @return {validator}
+     */
+    s.Or = function Or (a, b) {
+        return s.Any([ a, b ]); 
+    };
 
-/**
- * @nosideeffects
- * @param {number} lowerBound
- * @return {validator}
- */
-jsoSchema.GreaterThanEqual = function GreaterThanEqual (lowerBound) {
-    return jsoSchema.And(jsoSchema.Number(), jsoSchema.Condition(function (value) { return lowerBound <= value; }));
-};
+    s.If = function If (condition, then, els) {
+        return s.Or(s.And(condition, then), els);
+    };
 
-/**
- * @nosideeffects
- * @param {number} upperBound
- * @return {validator}
- */
-jsoSchema.LessThan = function LessThan (upperBound) {
-    return jsoSchema.And(jsoSchema.Number(), jsoSchema.Condition(function (value) { return value < upperBound; }));
-};
+    /** 
+     * Helper function for simple tests
+     * @param {function(*):boolean} valueCheck
+     * @return {validator}
+     */
+    s.Condition = function Condition (valueCheck) {
+        return function(value, p, f) {
+            return valueCheck(value) ? p() : f(value, " did not return true from ", valueCheck);
+        };
+    };
 
-/**
- * @nosideeffects
- * @param {number} upperBound
- * @return {validator}
- */
-jsoSchema.LessThanEqual = function LessThanEqual (upperBound) {
-    return jsoSchema.And(jsoSchema.Number(), jsoSchema.Condition(function (value) { return value <= upperBound; }));
-};
+    /**
+     * Return type, as string, of the passed in object. Distinguish
+     * between null, arrays and objects.
+     *
+     * as per http://javascript.crockford.com/remedial.html
+     *
+     * @param {*} value
+     * @return {string}
+     */
+    var typeOf = function typeOf(value) {
+        var s = typeof value;
+        if (s === 'object') {
+            if (value) {
+                if (Object.prototype.toString.call(value) == '[object Array]') {
+                    return 'array';
+                }
+            } else {
+                return 'null';
+            }
+        }
+        return s;
+    };
 
-/**
- * @nosideeffects
- * @return {validator}
- */
-jsoSchema.String = function String () {
-    return jsoSchema.OfType("string");
-};
+    /**
+     * @nosideeffects
+     * @param {typeName} string
+     * @return {validator}
+     */
+    s.OfType = function OfType (typeName) {
+        return s.Condition(function (value) { return typeOf(value) == typeName; });
+    };
 
-/**
- * @nosideeffects
- * @return {validator}
- */
-jsoSchema.Boolean = function Boolean () {
-    return jsoSchema.OfType("boolean");
-};
+    /**
+     * @nosideeffects
+     * @return {validator}
+     */
+    s.Number = function Number () {
+        return s.OfType("number");
+    };
 
-/**
- * @nosideeffects
- * @param {string|RegExp} re
- * @return {validator}
- */
-jsoSchema.RegExp = function RegExp (re) {
-    if (jsoSchema.typeOf(re) == "string") {
-        re = new RegExp(re);
-    }
-    return jsoSchema.And(jsoSchema.String(), jsoSchema.Condition(function (value) { return re.exec(value) != null; }));
-};
+    /**
+     * @nosideeffects
+     * @param {number} lowerBound
+     * @return {validator}
+     */
+    s.GreaterThan = function GreaterThan (lowerBound) {
+        return s.And(s.Number(), s.Condition(function (value) { return lowerBound < value; }));
+    };
 
-/**
- * @nosideeffects
- * @param {Array.<*>} values
- * @return {validator}
- */
-jsoSchema.Enum = function Enum (values) {
-    var set = { };
-    var i;
-    for (i = 0; i < values.length; i++) {
-        set[values[i]] = true;
-    }
-    return jsoSchema.Condition(function (value) { return set.hasOwnProperty(value); });
-};
+    /**
+     * @nosideeffects
+     * @param {number} lowerBound
+     * @return {validator}
+     */
+    s.GreaterThanEqual = function GreaterThanEqual (lowerBound) {
+        return s.And(s.Number(), s.Condition(function (value) { return lowerBound <= value; }));
+    };
 
-/**
- * @nosideeffects
- * @param {*} value
- * @return {validator}
- */
-jsoSchema.Constant = function Constant (value) {
-    return jsoSchema.Condition(function (v) { return v == value; });
-};
+    /**
+     * @nosideeffects
+     * @param {number} upperBound
+     * @return {validator}
+     */
+    s.LessThan = function LessThan (upperBound) {
+        return s.And(s.Number(), s.Condition(function (value) { return value < upperBound; }));
+    };
 
-jsoSchema.Object = (function () { 
+    /**
+     * @nosideeffects
+     * @param {number} upperBound
+     * @return {validator}
+     */
+    s.LessThanEqual = function LessThanEqual (upperBound) {
+        return s.And(s.Number(), s.Condition(function (value) { return value <= upperBound; }));
+    };
+
+    /**
+     * @nosideeffects
+     * @return {validator}
+     */
+    s.String = function String () {
+        return s.OfType("string");
+    };
+
+    /**
+     * @nosideeffects
+     * @return {validator}
+     */
+    s.Boolean = function Boolean () {
+        return s.OfType("boolean");
+    };
+
+    /**
+     * @nosideeffects
+     * @param {string|RegExp} re
+     * @return {validator}
+     */
+    s.RegExp = function RegExp (re) {
+        if (typeOf(re) == "string") {
+            re = new RegExp(re);
+        }
+        return s.And(s.String(), s.Condition(function (value) { return re.exec(value) != null; }));
+    };
+
+    /**
+     * @nosideeffects
+     * @param {Array.<*>} values
+     * @return {validator}
+     */
+    s.Enum = function Enum (values) {
+        var set = { };
+        var i;
+        for (i = 0; i < values.length; i++) {
+            set[values[i]] = true;
+        }
+        return s.Condition(function (value) { return set.hasOwnProperty(value); });
+    };
+
+    /**
+     * @nosideeffects
+     * @param {*} value
+     * @return {validator}
+     */
+    s.Constant = function Constant (value) {
+        return s.Condition(function (v) { return v == value; });
+    };
 
     var check_one_required_property = function(spec,
                                                required_properties, optional_properties, allow_other_properties,
@@ -223,7 +223,7 @@ jsoSchema.Object = (function () {
         var property = required_properties[0];
         var property_schema = spec.required_properties[property];
 
-        if (jsoSchema.typeOf(value[property]) == "undefined") {
+        if (typeOf(value[property]) == "undefined") {
             return f("Missing required property ", property, " on ", value);
         } else {
             return property_schema(value[property],
@@ -317,77 +317,79 @@ jsoSchema.Object = (function () {
         }
     };
 
-    return function Object (spec) {
+    s.Object = function Object (spec) {
         return function (value, p, f) {
             object_schema_loop(spec,
                                spec.required_properties || { },
                                spec.optional_properties || { },
-                               jsoSchema.typeOf(spec.allow_other_properties) == "undefined" ? true : spec.allow_other_properties,
+                               typeOf(spec.allow_other_properties) == "undefined" ? true : spec.allow_other_properties,
                                value,
                                p,
                                f);
         };
     };
 
-})();
+    s.Record = function Record (required_properties) {
+        return s.Object({ required_properties: required_properties, 
+                          optional_properties: { } });
+    };
 
-jsoSchema.Record = function Record (required_properties) {
-    return jsoSchema.Object({ required_properties: required_properties, 
-                                  optional_properties: { } });
-};
+    s.HashTable = function HashTable () {
+        return s.Object({ required_properties: { }, 
+                          optional_properties: { },
+                          allow_other_properties: true });
+    };
 
-jsoSchema.HashTable = function HashTable () {
-    return jsoSchema.Object({ required_properties: { }, 
-                                  optional_properties: { },
-                                  allow_other_properties: true });
-};
+    /**
+     * @param {s.validator} item_validator
+     * @return {s.validator}
+     */
+    s.Array = function Array (item_validator) {
+        if (arguments.length == 0) {
+            throw new Error("Missing required argument item_validator");
+        }
+        return function (value, p, f) {
+            var loop = function (index, p, f) {
+                if (index == value.length) {
+                    return p();
+                } else {
+                    return item_validator(value[index],
+                                          function () {
+                                              return loop(index + 1, p, f);
+                                          },
+                                          f);
+                }
+            };
 
-/**
- * @param {jsoSchema.validator} item_validator
- * @return {jsoSchema.validator}
- */
-jsoSchema.Array = function Array (item_validator) {
-    if (arguments.length == 0) {
-        throw new Error("Missing required argument item_validator");
-    }
-    return function (value, p, f) {
-        var loop = function (index, p, f) {
-            if (index == value.length) {
-                return p();
+            if (typeOf(value) == 'array') {
+                return loop(0, p, f);
             } else {
-                return item_validator(value[index],
-                                      function () {
-                                          return loop(index + 1, p, f);
-                                      },
-                                      f);
+                return f(value, "is not an array.");
             }
         };
-
-        if (jsoSchema.typeOf(value) == 'array') {
-            return loop(0, p, f);
-        } else {
-            return f(value, "is not an array.");
-        }
     };
-};
 
-jsoSchema.Nullable = function Nullable (validator) {
-    return jsoSchema.Or(jsoSchema.Constant(null), validator);
-};
+    s.Nullable = function Nullable (validator) {
+        return s.Or(s.Constant(null), validator);
+    };
 
-jsoSchema.Pass = function Pass () { return function (value,p,f) { return p(); }; };
-jsoSchema.Fail = function Fail () { var message = [].concat(arguments); return function (value,p,f) { return f.apply(f, message); }; };
+    s.Pass = function Pass () { return function (value,p,f) { return p(); }; };
+    s.Fail = function Fail () { var message = [].concat(arguments); return function (value,p,f) { return f.apply(f, message); }; };
 
-jsoSchema.violatesSchema = function violatesSchema (value, schema) {
-    var error = undefined;
-    var ok = false;
-    schema(value,
-           function () { ok = true; },
-           function () { error = { value: value, schema: schema, message: [].concat(arguments) }; });
+    s.violatesSchema = function violatesSchema (value, schema) {
+        var error = undefined;
+        var ok = false;
+        schema(value,
+               function () { ok = true; },
+               function () { error = { value: value, schema: schema, message: [].concat(arguments) }; });
 
-    return ok ? false : error;
-};
+        return ok ? false : error;
+    };
 
-jsoSchema.validate = function validate (value, schema) {
-    return jsoSchema.violatesSchema(value, schema) == false ? true : false;
-};
+    s.validate = function validate (value, schema) {
+        return s.violatesSchema(value, schema) == false ? true : false;
+    };
+
+    return s;
+
+})();
