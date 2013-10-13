bbdb2cf3f5183540cb2d9a381677b84b4c43040f
Marco Baringer <mb@bese.it>
2013-10-13 15:36:11 +0200

    Added LICENSE

--- src/jsoSchema.js
+++ src/jsoSchema.js
@@ -1,8 +1,17 @@
-/* A javascript (json) data structure parser/validator. 
+/*
+ * Copyright 2013 Edward Marco Baringer
  *
- * Combinators are writte in CPS style: Every combinator is passed a
- * value to check and a function to call if the value passes and a
- * second function to call if the value fails.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.  You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 var jsoSchema = (function () {
